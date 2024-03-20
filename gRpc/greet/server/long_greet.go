b83cd2452a5678e7f7685e5bb7a26d12152562c5
Luis Felipe <godoiluisfelipe@gmail.com>
2024-03-19 23:34:39 -0300

    feat: finally stream client

--- gRpc/greet/server/long_greet.go
+++ gRpc/greet/server/long_greet.go
@@ -27,6 +27,8 @@ func (s *Server) LongGreet(stream pb.GreetService_LongGreetServer) error {
 			log.Fatalf("Error while reading client stream: %v\n", err)
 		}
 
+		log.Printf("Receiving: %v\n", req)
+
 		res += fmt.Sprintf("Hello %s!\n", req.FirstName)
 	}
 }
