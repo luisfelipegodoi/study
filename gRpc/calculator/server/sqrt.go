db5931e0174efe61a7b058186879c0043b5e6c8b
Luis Felipe <godoiluisfelipe@gmail.com>
2024-03-29 23:06:56 -0300

    wip: errors imp

--- gRpc/calculator/server/sqrt.go
+++ gRpc/calculator/server/sqrt.go
@@ -1,7 +1,30 @@
 package main
 
-//import pb "github.com/luisfelipegodoi/study/gRpc/calculator/proto"
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
 
-func (s *Server) Sqrt() error {
+	pb "github.com/luisfelipegodoi/study/gRpc/calculator/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) Sqrt(ctx context.Context, in *pb.SqrtRequest) (*pb.SqrtResponse, error) {
+	log.Printf("Sqrt was invoked with: %v\n", in)
+
+	number := in.Number
+
+	if number < 0 {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			fmt.Sprintf("Received a negative number: %d\n", number),
+		)
+	}
+
+	return &pb.SqrtResponse{
+		Result: math.Sqrt(float64(number)),
+	}, nil
 
 }
