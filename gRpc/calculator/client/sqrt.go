1c19c0509b18c29669fd182c01866c8ace123eba
Luis Felipe <godoiluisfelipe@gmail.com>
2024-03-30 13:17:29 -0300

    feat: finnaly errors impl

--- gRpc/calculator/client/sqrt.go
+++ gRpc/calculator/client/sqrt.go
@@ -5,6 +5,8 @@ import (
 	"log"
 
 	pb "github.com/luisfelipegodoi/study/gRpc/calculator/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func doSqrt(c pb.CalculatorServiceClient, n int32) {
@@ -14,6 +16,20 @@ func doSqrt(c pb.CalculatorServiceClient, n int32) {
 		Number: n,
 	})
 	if err != nil {
+		e, ok := status.FromError(err)
 
+		if ok {
+			log.Printf("error message from server: %s\n", e.Message())
+			log.Printf("error code from server: %s\n", e.Code())
+
+			if e.Code() == codes.InvalidArgument {
+				log.Println("we problem sent a negative number")
+				return
+			}
+		} else {
+			log.Fatalf("a non gRpc error: %v\n", err)
+		}
 	}
+
+	log.Printf("Sqrt: %f\n", res.Result)
 }
