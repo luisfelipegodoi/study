e937e565286b6aa1f56a96552ba0d2864739efc6
Luis Felipe <godoiluisfelipe@gmail.com>
2024-04-03 00:17:57 -0300

    wip: deadlines

--- gRpc/greet/server/greet_with_deadline.go
+++ gRpc/greet/server/greet_with_deadline.go
@@ -3,10 +3,26 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	pb "github.com/luisfelipegodoi/study/gRpc/greet/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *Server) GreetWithDeadline(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
 	log.Printf("GreetWithDeadline was invoked with %v\n", in)
+
+	for i := 0; i < 3; i++ {
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("The client canceled the requisition")
+			return nil, status.Error(codes.Canceled, "The client canceled the request")
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return &pb.GreetResponse{
+		Result: "hello " + in.FirstName,
+	}, nil
 }
