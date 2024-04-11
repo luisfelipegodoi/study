8416c062248d513704d88e55990da9273218a046
Luis Felipe <godoiluisfelipe@gmail.com>
2024-04-10 23:22:25 -0300

    feat: added server impl for blog

---
+++ gRpc/blog/server/blog_item.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	pb "github.com/luisfelipegodoi/study/gRpc/blog/proto"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type BlogItem struct {
+	ID       primitive.ObjectID `bson:"_id, omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Title    string             `bson:"title"`
+	Content  string             `bson:"content"`
+}
+
+func documentToBlog(data *BlogItem) *pb.Blog {
+	return &pb.Blog{
+		Id:       data.ID.Hex(),
+		AuthorId: data.AuthorID,
+		Title:    data.Title,
+		Content:  data.Content,
+	}
+}
