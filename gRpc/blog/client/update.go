e2a76f979755d14cc3153b0acb6b37850dbc1a40
Luis Felipe <godoiluisfelipe@gmail.com>
2024-04-14 20:57:53 -0300

    wip: impl update blog client

--- gRpc/blog/client/update.go
+++ gRpc/blog/client/update.go
@@ -1,1 +1,17 @@
 package client
+
+import (
+	"log"
+
+	pb "github.com/luisfelipegodoi/study/gRpc/blog/proto"
+)
+
+func updateBlog(c pb.BlogServiceClient, id string) {
+	log.Println("--- updateBlog was invoked ---")
+
+	newBlog := &pb.Blog{
+		Id:       id,
+		AuthorId: "Luix",
+		Title:    "New Title of blog",
+	}
+}
