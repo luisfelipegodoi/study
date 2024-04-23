59d3b1f5311937633d5b5cdc18fa4150c08d9f43
Luis Felipe <godoiluisfelipe@gmail.com>
2024-04-22 23:58:00 -0300

    wip: profiling

---
+++ golang/profiling/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"sync"
+)
+
+func main() {
+	// we need a webserver to get the pprof webserver
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	log.Print("Hello world")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go leakyFunction(wg)
+	wg.Wait()
+}
